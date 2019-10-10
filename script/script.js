b2c328ee4156956a314c391d87fb8ffd83136c7b
VerbekeTibo <tibo.verbeke@student.howest.be>
2019-10-10 15:39:44 +0200

    first

---
+++ script/script.js
@@ -0,0 +1,12 @@
+let email = {},
+  password = {},
+  signInButton;
+
+  function myFunction() {
+    var x = document.getElementById("myInput");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
